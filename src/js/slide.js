0eacc196d67caaf579da841de4037401668fa7dc
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-02-22 18:42:02 +0200

    2й слайдер

--- src/js/slide.js
+++ src/js/slide.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
       prevArrow: '.arrowperv',
       nextArrow: '.arrownext',
     }); 
+   $('.slider_2').slick({
+     slidesToShow: 2,
+     autoplay: true,
+     autoplaySpeed: 2000,
+     variableWidth: true,
+     prevArrow: '.arrowperv_abouthotel',
+     nextArrow: '.arrownext_abouthotel',
+     rtl: true,
+   }); 
 });
